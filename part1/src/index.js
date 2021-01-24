23a4f457f6ff89a57e5d6bb2c0341ff159dbf3f3
Chapman-Zhao <chapmananderzhao@gmail.com>
2021-01-24 17:21:21 +0800

    Rendering collections

--- part1/src/index.js
+++ part1/src/index.js
@@ -1,57 +1,40 @@
-import React, { useState } from 'react'
+import React from 'react'
 import ReactDOM from 'react-dom'
 
-const Select = (props) => {
-  for (let i = 0; i < vote.length; i ++)
-    if (vote[i] > max) {
-      max = vote[i]
-      most = i
-    }
-  return (
-    <p>{props.anecdotes[most]}</p>
-  )
-}
+const notes = [
+  {
+    id: 1,
+    content: 'HTML is easy',
+    date: '2019-05-30T17:30:31.098Z',
+    important: true
+  },
+  {
+    id: 2,
+    content: 'Browser can execute only JavaScript',
+    date: '2019-05-30T18:39:34.091Z',
+    important: false
+  },
+  {
+    id: 3,
+    content: 'GET and POST are the most important methods of HTTP protocol',
+    date: '2019-05-30T19:20:14.298Z',
+    important: true
+  }
+]
 const App = (props) => {
-  const [selected, setSelected] = useState(0)
-
-  const handleSelectedClick = () => {
-    let num = Math.floor(Math.random()*10)
-    if (num < anecdotes.length)
-      setSelected(num)
-  }
-
-  const handleVoteClick = () => {
-    vote[selected] += 1
-  }
+  const { notes } = props
 
   return (
     <div>
-      <h1>Anecdotes of the day</h1>
-      {props.anecdotes[selected]}
-      <p>has {props.vote[selected]} votes</p>
-      <button onClick={handleVoteClick}>vote</button>
-      <button onClick={handleSelectedClick}>next</button>
-      <h1>Anecdotes with most vote</h1>
-      <Select anecdotes={anecdotes} />
+      <h1>Notes</h1>
+      <ul>
+        {notes.map(note => <li>{note.content}</li>)}
+      </ul>
     </div>
   )
 }
 
-const anecdotes = [
-  'If it hurts, do it more often',
-  'Adding manpower to a late software project makes it later!',
-  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-  'Premature optimization is the root of all evil.',
-  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
-]
-
-const vote = [0, 0, 0, 0, 0, 0]
-
-let most = 0
-let max = 0
-
 ReactDOM.render(
-  <App anecdotes={anecdotes} vote={vote} />,
+  <App notes={notes} />,
   document.getElementById('root')
 )
