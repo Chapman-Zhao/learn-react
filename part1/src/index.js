1bec31ce7557eac85588780a80e74f5f924b46b1
Chapman-Zhao <chapmananderzhao@gmail.com>
2021-01-24 17:38:51 +0800

    Refactoring modules

--- part1/src/index.js
+++ part1/src/index.js
@@ -21,14 +21,20 @@ const notes = [
     important: true
   }
 ]
-const App = (props) => {
-  const { notes } = props
 
+const Notes = ({ note }) => {
+  return (
+    <li>{note.content}</li>
+  )
+}
+
+const App = ({ notes }) => {
   return (
     <div>
       <h1>Notes</h1>
       <ul>
-        {notes.map(note => <li key={note.id}>{note.content}</li>)}
+        {notes.map(note => 
+          <Notes key={note.id} note={note} />)}
       </ul>
     </div>
   )
