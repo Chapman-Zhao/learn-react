b955ca4b9286f6a421bbbe00ad305e546dd6c337
Chapman-Zhao <chapmananderzhao@gmail.com>
2021-01-25 20:22:54 +0800

    finish exercises 2.6

--- part1/src/index.js
+++ part1/src/index.js
@@ -1,80 +1,52 @@
 import React, {useState} from 'react'
 import ReactDOM from 'react-dom'
-import Note from './components/Note'
 
-const App = (props) => {
-  const [notes, setNotes] = useState(props.notes)
-  const [newNote, setNewNote] = useState('')
-  const [showAll, setShowAll] = useState(true)
+const App = () => {
+  const [ persons, setPersons ] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+  const [ newName, setNewName ] = useState('')
 
-  const notesToShow = showAll
-    ? notes
-    : notes.filter(note => note.important === true)
-  
-  const addNote = (event) => {
-    event.preventDefault()
-    const noteObject = {
-      content: newNote,
-      date: new Date().toISOString(),
-      important: Math.random() < 0.5,
-      id: notes.length + 1,
-    }
-  
-    setNotes(notes.concat(noteObject))
-    setNewNote('')
-  }
-
-  const handleNoteChange = (event) => {
+  const handleNameChange = (event) => {
     console.log(event.target.value)
-    setNewNote(event.target.value)
+    setNewName(event.target.value)
   }
-  
+
+  const addName = (event) => {
+    event.preventDefault()
+    const nameObject ={
+      name: newName
+    }
+    console.log(nameObject)
+    setPersons(persons.concat(nameObject))
+    setNewName('')
+  }
+
   return (
     <div>
-      <h1>Notes</h1>
-      <div>
-        <button onClick={() => setShowAll(!showAll)}>
-          show {showAll ? 'important' : 'all'}
-        </button>
-      </div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addName}>
+        <div>
+          name: <input 
+            value={newName}
+            onChange={handleNameChange}
+          />
+        </div>
+        <div>
+          <button type='submit'>add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
       <ul>
-        {notesToShow.map(note => 
-          <Note key={note.id} note={note} />
+        {persons.map(person => 
+          <li key={person.name}>{person.name}</li>
         )}
       </ul>
-      <form onSubmit={addNote}>
-        <input 
-          value={newNote}
-          onChange={handleNoteChange}
-        />
-        <button type="submit">save</button>
-      </form>
     </div>
   )
 }
 
-const notes = [
-  {
-    id: 1,
-    content: 'HTML is easy',
-    date: '2019-05-30T17:30:31.098Z',
-    important: true
-  },
-  {
-    id: 2,
-    content: 'Browser can execute only JavaScript',
-    date: '2019-05-30T18:39:34.091Z',
-    important: false
-  },
-  {
-    id: 3,
-    content: 'GET and POST are the most important methods of HTTP protocol',
-    date: '2019-05-30T19:20:14.298Z',
-    important: true
-  }
-]
-
 ReactDOM.render(
-  <App notes={notes}/>,
+  <App />,
   document.getElementById('root')
 )
