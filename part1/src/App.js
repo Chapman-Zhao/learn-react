3df7bab496376d49785f670f85ec328224e26619
Chapman-Zhao <chapmananderzhao@gmail.com>
2021-01-29 15:11:27 +0800

    finish execrises 2.12

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,84 +1,40 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 
-import Filter from './components/Filter'
-import Persons from './components/Persons'
-import Personsform from './components/Personsform'
-
 const App = () => {
-  const [ persons, setPersons ] = useState([])
-  const [ filterPersons, setFilterPersons ] = useState([]) 
-  const [ newName, setNewName ] = useState('')
-  const [ newNumber, setNewNumber ] = useState('')
+  const [ countriesList, setCountriesList] = useState([])
+  const [ filterList, setFilterList] = useState([])
 
-  const hook = () => {
+  const effectHook = () => {
     axios
-    .get('http://localhost:3001/persons')
-    .then(response => {
-      console.log(response.data)
-      setPersons(response.data)
-    })
-  } 
-
-  useEffect(hook, [])
-
-  const handleNameChange = (event) => {
-    console.log(event.target.value)
-    setNewName(event.target.value)
+      .get('https://restcountries.eu/rest/v2/all')
+      .then(response => {
+        console.log(response.data)
+        setCountriesList(response.data)
+      })
   }
 
-  const handleNumberChange = (event) => {
-    console.log(event.target.value)
-    setNewNumber(event.target.value)
-  }
+  useEffect(effectHook, [])
 
-  const handleFilterChange = (event) => {
-    console.log(event.target.value)
-
+  const handleChange = (event) => {
     const search = event.target.value
-
-    setFilterPersons(
-      search ? (
-        persons.filter(t => t.name.includes(search))
-      )      : persons
-    )
-  }
-
-  const addName = (event) => {
-    event.preventDefault()
-    const nameObject ={
-      name: newName,
-      number: newNumber
-    }
-    let flag = 0
-    persons.forEach((item) => {
-      if (item.name === nameObject.name) flag = 1
-    })
-    console.log(nameObject)
-    if (flag !== 1) {
-      setPersons(persons.concat(nameObject))
-    } else {
-      flag = 0
-      window.alert(nameObject.name + " is already added to phonebook")
-    }
-    setNewName('')
-    setNewNumber('')
+    search ? (
+      setFilterList(
+        countriesList.filter(name => name.includes(search))
+      )
+    ) : countriesList
   }
 
   return (
     <div>
-      <h2>Phonebook</h2>
-      <Filter onchange={handleFilterChange} />
-      <h2>Add a New</h2>
-      <Personsform 
-        onSubmit={addName} 
-        newName={newName} 
-        newNumber={newNumber} 
-        onNameChange={handleNameChange}
-        onNumberChange={handleNumberChange}
+      Find Countries: <input 
+        onChange={handleChange}
       />
-      <h2>Numbers</h2>
-      <Persons filterPersons={filterPersons}/>
+      <ul>
+        {filterList.map(country =>
+          <li key={country.name}>{country.name}</li>
+        )}
+      </ul>
     </div>
   )
 }
